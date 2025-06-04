910e3bd367f0a65d9dd2973c0458441681c5b0eb
gamgamiRayan <rayan.gamgami@gmail.com>
2025-06-04 17:01:14 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -67,4 +67,4 @@ def afficher_cv():
     return render_template('cv.html')
                                                                                                                
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=5000)
